7753b7d5274f427c94d2ffe67f032c4a90fc1d6b
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-10 23:01:05 +0700

    Memproses kode Sass
    - Bundling file assets dan bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 import './bootstrap';
+import * as bootstrap from 'bootstrap';
 import './script1';
 import './script2';
-import '../css/app.css';
+// import '../css/app.css';
+
+new bootstrap.Popover(document.getElementById('myPopover'))
 
 // Tampilkan alert ketika halaman di klik
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite('resources/js/app.js')
+    {{-- @vite('resources/sass/style.scss') --}}
+    @vite('resources/sass/app.scss')
 </head>
+
 <body>
-    <h1>Belajar Laravel</h1>
+    {{-- <h1>Belajar Laravel</h1> --}}
+    <div class="container text-center py-4">
+        <h1>Belajar Laravel</h1>
+        <button id="myPopover" type="button" class="btn btn-lg btn-danger mt-4" data-bs-toggle="popover"
+            title="Lagi serius..." data-bs-content="Buku Laravel Uncover top bgt!">
+            Belajar Laravel </button>
+    </div>
+    @vite('resources/js/app.js')
 </body>
+
 </html>
