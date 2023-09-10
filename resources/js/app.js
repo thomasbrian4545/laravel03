b78911428d678126c73b74a81574dfeee1561a36
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-10 21:39:30 +0700

    Import ke file "Master"

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import './bootstrap';
+import './script1';
+import './script2';
 
 // Tampilkan alert ketika halaman di klik
 
-window.addEventListener('click', function () {
-    alert('Saya di klik');
-});
+// window.addEventListener('click', function () {
+//     alert('Saya di klik');
+// });
--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/style1.css',
-                'resources/css/style2.css',
-                'resources/js/script1.js',
-                'resources/js/script2.js'],
+            input: ['resources/css/app.css',
+                'resources/js/app.js'],
             refresh: true,
         }),
     ],
